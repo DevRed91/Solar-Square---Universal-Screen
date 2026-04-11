9d191b0783325d638ce74dc7003daf12a7d0502e
Devraj Reddy <51876334+DevRed91@users.noreply.github.com>
2026-04-11 12:25:28 +0000

    update: Your City added

--- src/data/constant.tsx
+++ src/data/constant.tsx
@@ -560,12 +560,12 @@ export const WIND_DATA: WindDataType[] = [
   {
     heading: 'Breeze',
     description:
-      'Everyday monsoon breeze — like a typical rainy day in Nagpur.',
+      'Everyday monsoon breeze — like a typical rainy day in Your City.',
   },
   {
     heading: 'Strong Winds',
     description:
-      'Shakes windows and breaks weak branches — common in Nagpur’s peak monsoon.',
+      'Shakes windows and breaks weak branches — common in Your City\'s peak monsoon.',
   },
   {
     heading: 'Storm',
